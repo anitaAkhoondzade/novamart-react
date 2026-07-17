6c557b0661afd5447b931a9be6a708b799f29007
anita <anitaakhoondzade77@gmail.com>
2026-07-17 11:28:19 +0330

    feat: Display featured products

---
+++ src/components/home/FeaturedProducts.jsx
@@ -0,0 +1,23 @@
+import ProductCard from "../product/ProductCard";
+
+export default function FeaturedProducts({products}) {
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto px-8 py-20">
+        <h2 className="text-3xl font-bold mb-8">Featured Products</h2>
+        <div
+          className="grid
+  grid-cols-1
+  gap-4
+  md:grid-cols-2
+  lg:grid-cols-3
+  xl:grid-cols-4"
+        >
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
