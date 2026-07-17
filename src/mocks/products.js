5b1d60c6d6367dcd3f1ca86a8ae322b288df284c
anita <anitaakhoondzade77@gmail.com>
2026-07-17 11:27:03 +0330

    feat: Add mock product data

---
+++ src/mocks/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    title: "iPhone 16",
+    price: 999,
+    image: "https://placehold.co/400x400",
+    rating: {
+      rate: 5,
+      count: 120,
+    },
+  },
+  {
+    id: 2,
+    title: "MacBook Air",
+    price: 1299,
+    image: "https://placehold.co/400x400",
+    rating: {
+      rate: 4,
+      count: 85,
+    },
+  },
+  {
+    id: 3,
+    title: "AirPods Pro",
+    price: 249,
+    image: "https://placehold.co/400x400",
+    rating: {
+      rate: 5,
+      count: 240,
+    },
+  },
+];
+
+export default products;
