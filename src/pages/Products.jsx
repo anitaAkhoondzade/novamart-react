d10c048129219b8d6c7f78f9cc9c959c50ab7a5b
anita <anitaakhoondzade77@gmail.com>
2026-07-21 14:52:43 +0330

    feat: add product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,26 @@
-export default function Products(){
-    return <h2>Products page</h2>
+import EmptyState from "../components/common/EmptyState";
+import ErrorState from "../components/common/ErrorState";
+import Loading from "../components/common/Loading";
+import ProductsGrid from "../components/product/ProductsGrid";
+import { useProducts } from "../hooks/useProducts";
+
+export default function Products() {
+  const {products, loading, error} = useProducts();
+
+  {
+    if (loading) return <Loading />;
+  }
+  {
+    if (error) return <ErrorState />;
+  }
+  {
+    if (products.length === 0) return <EmptyState />;
+  }
+
+  return (
+    <>
+      <h1>All Products</h1>
+      <ProductsGrid products={products} />
+    </>
+  );
 }
