cfe221ae72758a51ae54401bb03e4fb6a616d3ae
anita <anitaakhoondzade77@gmail.com>
2026-07-22 18:26:33 +0330

    refactor: simplify ProductsGrid

--- src/components/product/ProductsGrid.jsx
+++ src/components/product/ProductsGrid.jsx
@@ -1,23 +1,18 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductsGrid({products}) {
+export default function ProductsGrid({ products }) {
   return (
-    <section>
-      <div className="max-w-7xl mx-auto px-8 py-20">
-        <h2 className="text-3xl font-bold mb-8">Featured Products</h2>
-        <div
-          className="grid
+    <div
+      className="grid
   grid-cols-1
   gap-4
   md:grid-cols-2
   lg:grid-cols-3
   xl:grid-cols-4"
-        >
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
-    </section>
+    >
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,7 @@ import EmptyState from "../components/common/EmptyState";
 import { useProducts } from "../hooks/useProducts";
 
 export default function Home() {
-  
-  const {products, loading, error} = useProducts()
+  const { products, loading, error } = useProducts();
 
   if (loading) return <Loading />;
 
@@ -20,7 +19,13 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <ProductsGrid products={products} />
+
+      <section>
+        <div className="max-w-7xl mx-auto px-8 py-20">
+          <h2 className="text-3xl font-bold mb-8">Featured Products</h2>
+          <ProductsGrid products={products} />
+        </div>
+      </section>
     </>
   );
 }
