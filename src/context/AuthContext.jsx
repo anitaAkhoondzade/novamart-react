d5ccc942ca06878c91c2c37690c4e581489fe70b
anita <anitaakhoondzade77@gmail.com>
2026-08-07 11:54:46 +0330

    feat: persist authentication state

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,9 +3,21 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) return JSON.parse(savedUser);
+    else return null;
+  });
 
   const isAuthenticated = user !== null;
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
 
   function login(email, password) {
     if (email && password) setUser({ email });
@@ -30,7 +42,9 @@ export function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, login, register, logout }}>
+    <AuthContext.Provider
+      value={{ user, isAuthenticated, login, register, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
